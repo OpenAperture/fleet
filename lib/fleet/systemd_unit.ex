17c49b6d3a78bd97f74a4d3058dc3452f219bb75
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-05-21 16:39:02 -0500

    more logging

--- lib/fleet/systemd_unit.ex
+++ lib/fleet/systemd_unit.ex
@@ -23,7 +23,10 @@ defmodule OpenAperture.Fleet.SystemdUnit do
 
   @spec get_fleet_api(String.t) :: pid
   defp get_fleet_api(etcd_token) do
-    FleetApiInstances.get_instance(etcd_token)
+    Logger.debug("Retrieving FleetApi instance...")
+    instance = FleetApiInstances.get_instance(etcd_token)
+    Logger.debug("Found FleetApi instance...")
+    instance
   end
 
   @spec build_unit(String.t, FleetApi.Unit.t, FleetApi.UnitState.t) :: SystemdUnit.t
@@ -102,43 +105,57 @@ defmodule OpenAperture.Fleet.SystemdUnit do
   """
   @spec get_units(String.t()) :: List
   def get_units(etcd_token) do
-    Logger.debug("Retrieving units on cluster #{etcd_token}...")
-    api = get_fleet_api(etcd_token)
-    units = case FleetApi.Etcd.list_units(api) do
-      {:ok, nil} -> 
-        Logger.error("FleetApi returned invalid units for cluster #{etcd_token}")
-        nil
-      {:ok, units} -> units
-      {:error, reason} -> 
-        Logger.error("Failed to retrieve units on cluster #{etcd_token}:  #{inspect reason}")
-        nil
-    end
+    try do
+      Logger.debug("Retrieving units on cluster #{etcd_token}...")
+      api = get_fleet_api(etcd_token)
 
-    Logger.debug("Retrieving unit states on cluster #{etcd_token}...")
-    unit_states_by_name = case FleetApi.Etcd.list_unit_states(api) do
-      {:ok, nil} -> 
-        Logger.error("FleetApi returned invalid unit states for cluster #{etcd_token}")
-        %{}        
-      {:ok, unit_states} -> 
-        Enum.reduce unit_states, %{}, fn(unit_state, unit_states_by_name) ->
-          Map.put(unit_states_by_name, unit_state.name, unit_state)
+      Logger.debug("Executing list-units call to #{etcd_token}...")
+      units = case FleetApi.Etcd.list_units(api) do
+        {:ok, nil} -> 
+          Logger.error("FleetApi returned invalid units for cluster #{etcd_token}")
+          nil
+        {:ok, units} -> units
+        {:error, reason} -> 
+          Logger.error("Failed to retrieve units on cluster #{etcd_token}:  #{inspect reason}")
+          nil
+      end
+
+      Logger.debug("Retrieving unit states on cluster #{etcd_token}...")
+      unit_states_by_name = case FleetApi.Etcd.list_unit_states(api) do
+        {:ok, nil} -> 
+          Logger.error("FleetApi returned invalid unit states for cluster #{etcd_token}")
+          %{}        
+        {:ok, unit_states} -> 
+          Enum.reduce unit_states, %{}, fn(unit_state, unit_states_by_name) ->
+            Map.put(unit_states_by_name, unit_state.name, unit_state)
+          end
+        {:error, reason} -> 
+          Logger.error("Failed to retrieve unit states on cluster #{etcd_token}:  #{inspect reason}")
+          %{}
+      end     
+
+      if units == nil do
+        Logger.debug("There were no valid units found on cluster cluster #{etcd_token}, resolving SystemdUnit units via unit states...")
+        Enum.reduce Map.values(unit_states_by_name), [], fn(unit_state, systemd_units) ->
+          systemd_units ++ [build_unit(etcd_token, nil, unit_state)]
         end
-      {:error, reason} -> 
-        Logger.error("Failed to retrieve unit states on cluster #{etcd_token}:  #{inspect reason}")
-        %{}
-    end 
-
-    if units == nil do
-      Logger.debug("There were no valid units found on cluster cluster #{etcd_token}, resolving SystemdUnit units via unit states...")
-      Enum.reduce Map.values(unit_states_by_name), [], fn(unit_state, systemd_units) ->
-        systemd_units ++ [build_unit(etcd_token, nil, unit_state)]
+      else
+        Logger.debug("Resolving SystemdUnit units...")
+        Enum.reduce units, [], fn(unit, systemd_units) ->
+          systemd_units ++ [build_unit(etcd_token, unit, unit_states_by_name[unit.name])]
+        end
       end
-    else
-      Logger.debug("Resolving SystemdUnit units...")
-      Enum.reduce units, [], fn(unit, systemd_units) ->
-        systemd_units ++ [build_unit(etcd_token, unit, unit_states_by_name[unit.name])]
-      end
-    end
+    catch
+      :exit, code   -> 
+        Logger.error("[SystemdUnit] Failed to retrieve fleet api for cluster #{etcd_token}:  Exited with code #{inspect code}")
+        nil
+      :throw, value -> 
+        Logger.error("[SystemdUnit] Failed to retrieve fleet api for cluster #{etcd_token}:  Throw called with #{inspect value}")
+        nil
+      what, value   -> 
+        Logger.error("[SystemdUnit] Failed to retrieve fleet api for cluster #{etcd_token}:  Caught #{inspect what} with #{inspect value}")
+        nil
+    end             
   end
 
   @doc """
