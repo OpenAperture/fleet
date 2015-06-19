714c52b5258f96e741b57c73577431caa6f480b3
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-06-19 12:19:02 -0500

    switch to creating new instances rather than using cached instances

--- lib/fleet/fleet_api_instances.ex
+++ lib/fleet/fleet_api_instances.ex
@@ -2,20 +2,20 @@ require Logger
 
 defmodule OpenAperture.Fleet.FleetApiInstances do
     
-    def start_link(_opts \\ []) do
-        Agent.start_link(fn -> %{} end, name: __MODULE__)
-    end
+  def start_link(_opts \\ []) do
+      Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
 
-    def get_instance(etcd_token) do
-        Agent.get_and_update(__MODULE__, fn map ->
-          case map[etcd_token] do
-            nil -> 
-                {:ok, pid} = FleetApi.Etcd.start_link(etcd_token)
-                Logger.debug "created new fleet api instance for token #{etcd_token}"
-                {pid, Map.put(map, etcd_token, pid)}
-            pid ->
-                {pid, map}
-          end
-        end)
-    end
+  def get_instance(etcd_token) do
+    Agent.get_and_update(__MODULE__, fn map ->
+      case map[etcd_token] do
+        nil -> 
+          {:ok, pid} = FleetApi.Etcd.start_link(etcd_token)
+          Logger.debug "created new fleet api instance for token #{etcd_token}"
+          {pid, Map.put(map, etcd_token, pid)}
+        pid ->
+          {pid, map}
+      end
+    end)
+  end
 end
